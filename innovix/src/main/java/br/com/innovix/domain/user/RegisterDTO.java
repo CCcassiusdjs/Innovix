3650d62a02fc34f0beb583582d5142b1347e05f1
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2024-05-12 16:53:53 -0300

    Unused import removed

--- innovix/src/main/java/br/com/innovix/domain/user/RegisterDTO.java
+++ innovix/src/main/java/br/com/innovix/domain/user/RegisterDTO.java
@@ -2,7 +2,6 @@ package br.com.innovix.domain.user;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
